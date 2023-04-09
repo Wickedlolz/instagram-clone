301c79e02761907597fe458918c182452991bc6e
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-04-09 11:40:16 +0300

    fix routes and change title of index.html

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import { PostProvider } from './contexts/PostContext';
 
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Messages from './pages/Messages';
 import NotFound from './pages/NotFound';
+import AuthGuard from './utils/guards/AuthGuard';
+import UserProfile from './pages/UserProfile';
 
 import './App.css';
 
@@ -13,13 +16,43 @@ function App() {
     return (
         <FirebaseProvider>
             <PostProvider>
-                <Layout>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/messages" element={<Messages />} />
-                        <Route path="/*" element={<NotFound />} />
-                    </Routes>
-                </Layout>
+                <Routes>
+                    <Route element={<AuthGuard />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route
+                            path="/"
+                            element={
+                                <Layout>
+                                    <Home />
+                                </Layout>
+                            }
+                        />
+                        <Route
+                            path="/messages"
+                            element={
+                                <Layout>
+                                    <Messages />
+                                </Layout>
+                            }
+                        />
+                        <Route
+                            path="/profile"
+                            element={
+                                <Layout>
+                                    <UserProfile />
+                                </Layout>
+                            }
+                        />
+                        <Route
+                            path="/*"
+                            element={
+                                <Layout>
+                                    <NotFound />
+                                </Layout>
+                            }
+                        />
+                    </Route>
+                </Routes>
             </PostProvider>
         </FirebaseProvider>
     );
