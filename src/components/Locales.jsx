5d3b2546142529aa19a94b72b66bd01678031872
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-08-22 15:27:42 +0300

    fix locales

--- src/components/Locales.jsx
+++ src/components/Locales.jsx
@@ -1,26 +1,22 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { IntlProvider } from 'react-intl';
 
-// import useConfig from 'hooks/useConfig';
+const loadLocaleData = (locale) => {
+    switch (locale) {
+        case 'bg-BG':
+        case 'en-US':
+        default:
+            return import('../utils/localization/en.json');
+    }
+};
 
 const Locales = ({ children }) => {
-    // const { i18n } = useConfig();
-
     const [messages, setMessages] = useState();
 
     useEffect(() => {
         loadLocaleData(navigator.language).then((d) => {
             setMessages(d.default);
         });
-    }, []);
-
-    const loadLocaleData = useCallback((locale) => {
-        switch (locale) {
-            case 'bg-BG':
-            case 'en-US':
-            default:
-                return import('../utils/localization/en.json');
-        }
     }, []);
 
     return (
