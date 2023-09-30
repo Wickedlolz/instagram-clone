6400a8571330dd86dea182f58ca64e8c134a9274
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-30 20:10:33 +0300

    add interface for product

---
+++ src/interfaces/product.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+    _id: number;
+    title: string;
+    isNew: boolean;
+    oldPrice: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: number;
+    quantity: number;
+}
