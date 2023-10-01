906f7b6923d62fadf45abe231ac3b038f3e16f17
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-01 09:07:26 +0300

    add redux

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
+import { store } from '@/store';
 import { ToastContainer } from 'react-toastify';
 
 type LayoutProps = {
@@ -9,21 +11,23 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
     return (
-        <SessionProvider>
-            {children}
-            <ToastContainer
-                position="top-center"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="dark"
-            />
-        </SessionProvider>
+        <Provider store={store}>
+            <SessionProvider>
+                {children}
+                <ToastContainer
+                    position="top-center"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="dark"
+                />
+            </SessionProvider>
+        </Provider>
     );
 };
 
---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import productsReducer from './slices/productsSlice';
+
+export const store = configureStore({
+    reducer: {
+        shopping: productsReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
