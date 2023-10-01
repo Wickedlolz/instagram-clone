4742c5fb1590285c7f058750223516b34bf81f43
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-01 09:54:29 +0300

    add get id from search params and get product

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -2,32 +2,22 @@ import { IProduct } from '@/interfaces/product';
 import Container from '@/components/Container';
 import Product from '@/components/Product';
 import SingleProduct from '@/components/SingleProduct';
-import { getTrendingProducts } from '@/utils';
+import { getSingleProudct, getTrendingProducts } from '@/utils';
 
 type ProductPageProps = {
     searchParams: { [key: string]: string | string[] | undefined };
 };
 
 const ProductPage = async ({ searchParams }: ProductPageProps) => {
-    const product: IProduct = {
-        _id: 1,
-        title: 'Long sleeve Jacket',
-        isNew: true,
-        oldPrice: '200',
-        price: 150,
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla non magni facili blanditiis molestias soluta eveniet illum accusantium eius mollitia eligendi, ex iste doloribus magnam.',
-        category: 'women',
-        image: 'https://img.freepik.com/free-photo/portrait-beautiful-face-young-woman-with-long-brown-hair_186202-4331.jpg?size=626&ext=jpg&ga=GA1.1.453157835.1694346094&semt=sph',
-        rating: 4,
-        quantity: 1,
-    };
+    const _idString = searchParams?._id;
+    const _id = Number(_idString);
+    const product = getSingleProudct(_id);
     const data: IProduct[] = await getTrendingProducts();
 
     return (
         <div>
             <Container>
-                <SingleProduct product={product} />
+                <SingleProduct product={product!} />
                 <div>
                     <p className="text-xl py-1 font-semibold">
                         Tranding Products
