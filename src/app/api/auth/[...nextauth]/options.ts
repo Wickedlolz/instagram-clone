c5de1c8ec36b18748f5dd60efd5704b10f3b7844
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-10-03 16:59:27 +0300

    remove: credentials provider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,6 @@
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const options: NextAuthOptions = {
     providers: [
@@ -12,32 +11,6 @@ export const options: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                username: {
-                    label: 'username',
-                    type: 'text',
-                    placeholder: 'Enter your username',
-                },
-                password: {
-                    label: 'password',
-                    type: 'password',
-                    placeholder: 'Enter your password',
-                },
-            },
-            async authorize(credentials) {
-                const user = { id: '1001', name: 'Viktor', password: '123123' };
-                if (
-                    credentials?.username === user.name &&
-                    credentials?.password === user.password
-                ) {
-                    return user;
-                }
-
-                return null;
-            },
         }),
     ],
 };
